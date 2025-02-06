1ef0303769d65b597f1c13aea875b46ddb59684b
carlosvillaar <carlosvillar004@gmail.com>
2025-02-06 14:03:38 +0100

    feat: add errors handler and not found middlewares

--- controllers/mooviesController.js
+++ controllers/mooviesController.js
@@ -1,52 +1,57 @@
-const { response } = require('express');
-const connection = require('../data/db')
+const connection = require("../data/db");
 
 const index = (req, res) => {
   const sql = `
   SELECT * 
-  FROM movies`
+  FROM movies`;
   connection.query(sql, (err, results) => {
-    if (err) res.status(500).json({error: 'query al db fallita'});
-    res.json(results)
-  })
-}
+    if (err) res.status(500).json({ error: "query al db fallita" });
+    res.json(results);
+  });
+};
 
 const show = (req, res) => {
   const id = req.params.id;
-  
+
   const sql = `
   SELECT * 
   FROM movies M
-  WHERE M.id = ?`
+  WHERE M.id = ?`;
 
   const sqlReviews = `
   SELECT *
   FROM reviews R
   WHERE R.movie_id = ?
-  `
+  `;
 
-  const imagePath = 'http://localhost:3000/img/movies_cover/'
+  const imagePath = "http://localhost:3000/img/movies_cover/";
 
   connection.query(sql, [id], (err, results) => {
-    if (err) res.status(500).json({error: 'query al db fallita'});
-    if (results.length === 0) res.status(404).json({error: 'film non trovato'});
+    if (err) res.status(500).json({ error: "query al db fallita" });
+
+    if (results.length === 0)
+      res.status(404).json({ error: "film non trovato" });
+
     const moovie = results[0];
 
-    connection.query(sqlReviews, [id], (err, response) => {
-      if (err) res.status(500).json({error: 'query al db fallita'});
-      if (results.length === 0) res.status(404).json({error: 'film non trovato'});
-      const reviews = response
-      res.json({
-      ...moovie,
-      image: imagePath + moovie.title + '.jpg',
-      reviews
-    })
-    })
-  })
-}
+    connection.query(sqlReviews, [id], (err, results) => {
+      if (err) res.status(500).json({ error: "query al db fallita" });
 
+      if (results.length === 0 || results.id === null)
+        res.status(404).json({ error: "film non trovato" });
+
+      const reviews = results;
+
+      res.json({
+        ...moovie,
+        image: imagePath + moovie.title + ".jpg",
+        reviews,
+      });
+    });
+  });
+};
 
 module.exports = {
   index,
-  show
-}
+  show,
+};
--- server.js
+++ server.js
@@ -6,15 +6,24 @@ const port = 3000;
 
 const mooviesRouter = require('./router/moovies')
 
+const errorsHandler = require('./middlewares/errorshandlerr');
+
+const notFound = require('./middlewares/notFound')
+
 app.use(express.static('public'))
 
 app.use(express.json());
+
 
 app.get('/', (req, res) => {
   res.send('Server dei film')
 });
 
-app.use('/moovies', mooviesRouter)
+app.use('/movies', mooviesRouter)
+
+app.use(errorsHandler);
+
+app.use(notFound)
 
 app.listen(port, () => {
   console.log('sono in ascolto alla porta 3000')

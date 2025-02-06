47c2c7b3246835c2cb033df33a5d745b169e7ebb
carlosvillaar <carlosvillar004@gmail.com>
2025-02-06 13:04:21 +0100

    feat: add show route with images and reviws

--- controllers/mooviesController.js
+++ controllers/mooviesController.js
@@ -1,7 +1,10 @@
+const { response } = require('express');
 const connection = require('../data/db')
 
 const index = (req, res) => {
-  const sql = 'SELECT * FROM movies'
+  const sql = `
+  SELECT * 
+  FROM movies`
   connection.query(sql, (err, results) => {
     if (err) res.status(500).json({error: 'query al db fallita'});
     res.json(results)
@@ -10,7 +13,36 @@ const index = (req, res) => {
 
 const show = (req, res) => {
   const id = req.params.id;
-  res.send(`dettaglio film con id ${id}`)
+  
+  const sql = `
+  SELECT * 
+  FROM movies M
+  WHERE M.id = ?`
+
+  const sqlReviews = `
+  SELECT *
+  FROM reviews R
+  WHERE R.movie_id = ?
+  `
+
+  const imagePath = 'http://localhost:3000/img/movies_cover/'
+
+  connection.query(sql, [id], (err, results) => {
+    if (err) res.status(500).json({error: 'query al db fallita'});
+    if (results.length === 0) res.status(404).json({error: 'film non trovato'});
+    const moovie = results[0];
+
+    connection.query(sqlReviews, [id], (err, response) => {
+      if (err) res.status(500).json({error: 'query al db fallita'});
+      if (results.length === 0) res.status(404).json({error: 'film non trovato'});
+      const reviews = response
+      res.json({
+      ...moovie,
+      image: imagePath + moovie.title + '.jpg',
+      reviews
+    })
+    })
+  })
 }
 
 
